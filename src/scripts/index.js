ed72a75115823553a239077e30b828625dbf52a4
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-10-04 22:12:25 +0200

    Exercise concluded.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,44 @@
-// The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 import "babel-polyfill";
 
+const form = document.getElementById("form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const message = document.querySelector("#message");
+const checkbox = document.querySelector("#checkbox");
+const button = document.querySelector("button");
 
-// \/ All of your javascript should go here \/
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  try {
+    const prePayload = new FormData(form);
+    const payload = new URLSearchParams(prePayload);
+
+    const settings = {
+      method: "POST",
+      body: payload,
+    };
+
+    const response = fetch(
+      "https://jsonplaceholder.typicode.com/posts",
+      settings
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err.message));
+
+    console.log(response);
+
+    alert("Your form has been submitted. ");
+
+    name.value = "";
+    email.value = "";
+    password.value = "";
+    message.value = "";
+    checkbox.checked = false;
+  } catch (err) {
+    console.log("Error: ", err.message);
+  }
+});
